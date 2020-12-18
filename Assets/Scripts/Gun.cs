f22002ccf4f9e0dcdad31218ba31fcf89bed8bae
tylergreen1993 <tyler-green@live.com>
2020-12-18 17:10:31 -0600

    more work

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Pedestrian pedestrian = hit.collider.GetComponent<Pedestrian>();
+                if (pedestrian != null)
+                {
+                    StartCoroutine(KillPedestrian(pedestrian, hit.point));
+                }
+            }
+        }
+    }
+
+    private IEnumerator KillPedestrian(Pedestrian pedestrian, Vector3 origin)
+    {
+        yield return new WaitForSeconds(0.25f);
+
+        pedestrian.Die(transform.forward, origin);
+    }
+}
--- Assets/Scripts/Pedestrian.cs
+++ Assets/Scripts/Pedestrian.cs
@@ -3,13 +3,20 @@ using UnityEngine.AI;
 
 public class Pedestrian : MonoBehaviour
 {
+    public GameObject Blood;
     public bool IsWalking;
 
     private Vector3 _startPosition;
     private NavMeshAgent _navMeshAgent;
+    private bool _isDead;
 
     void Start()
     {
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.isKinematic = true;
+        }
+
         if (IsWalking)
         {
             _startPosition = transform.position;
@@ -18,8 +25,41 @@ public class Pedestrian : MonoBehaviour
         }
     }
 
+    public void Die(Vector3 bulletDirection, Vector3 bloodOrigin)
+    {
+        GameObject bloodSplatter = Instantiate(Blood, bloodOrigin, Quaternion.identity);
+        Destroy(bloodSplatter, 5);
+
+        if (_isDead)
+        {
+            return;
+        }
+
+        _isDead = true;
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Animator>().enabled = false;
+
+        if (IsWalking)
+        {
+            _navMeshAgent.enabled = false;
+        }
+
+        foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+        {
+            rb.isKinematic = false;
+            rb.AddForce(bulletDirection * 5, ForceMode.Impulse);
+        }
+
+        Destroy(gameObject, 10);
+    }
+
     private void Update()
     {
+        if (_isDead)
+        {
+            return;
+        }
+
         if (IsWalking && _navMeshAgent.remainingDistance < 0.1f)
         {
             _navMeshAgent.destination = Vector3.Distance(_navMeshAgent.destination, _startPosition) < 1f ? _startPosition - (Vector3.forward * 25) : _startPosition;
